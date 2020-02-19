e9fb5d65ea392b741ab6fd2211a650edbb3d1563
fredrick kamau <fredrickkamau10@outlook.com>
2020-02-19 17:58:04 +0300

    Set up project skeleton project webapi v2.2

--- ElectronicHealthRecords.ClientApp/ClientApp/src/app/app.component.spec.ts
+++ ElectronicHealthRecords.ClientApp/ClientApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ClientAPP'`, () => {
+  it(`should have as title 'ClientApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ClientAPP');
+    expect(app.title).toEqual('ClientApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ClientAPP app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ClientApp app is running!');
   });
 });
--- ElectronicHealthRecords.ClientApp/Program.cs
+++ ElectronicHealthRecords.ClientApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ElectronicHealthRecords.WebApi
+namespace ElectronicHealthRecords.ClientApp
 {
     public class Program
     {
